cbf9975342fa66ae94420b3fc91263604481e5f0
Kevin Takla <kevin.takla@nventive.com>
2024-05-06 19:39:59 -0400

    test: ensure test still works

--- src/app/test/app_test.dart
+++ src/app/test/app_test.dart
@@ -1,9 +1,10 @@
 import 'package:app/app.dart';
 import 'package:app/business/dad_jokes/dad_jokes_service.dart';
 import 'package:app/business/diagnostics/diagnostics_service.dart';
-import 'package:app/app_shell.dart';
+import 'package:app/shell.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:mockito/annotations.dart';
 
 import 'app_test.mocks.dart';
@@ -26,10 +27,12 @@ void main() {
     GetIt.I.registerSingleton<DadJokesService>(
       dadJokesService,
     );
+
+    GetIt.I.registerSingleton(Logger());
   });
 
   testWidgets('Shell Test', (WidgetTester tester) async {
     await tester.pumpWidget(const App());
-    expect(find.byType(AppShell), findsOneWidget);
+    expect(find.byType(Shell), findsOneWidget);
   });
 }
